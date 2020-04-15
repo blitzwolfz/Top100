762a749fcdf49c3506489eff25260667135c017a
blitzwolfz <qureshisamin25@gmail.com>
2020-04-15 16:04:05 -0400

    fourth

--- scrapper.py
+++ scrapper.py
@@ -11,28 +11,28 @@ def scrapper(subb):
     #basically scrapes the sub
     alist, lst, lst2 = [], [], []
     sub = reddit.subreddit(subb)
-    count = 0
     for submission in sub.top("all", limit=100):
         alist.append(submission.author)
         lst.append(str(submission.title))
         lst2.append(str(submission.score))
+    
+    lst3 = [alist, lst, lst2]
+    return lst3
 
-    return alist, lst, lst2
 
-
-def writer(alist, lst, lst2):
+def writer(alist):
     with open('Top100.csv', 'w', newline='') as file:
                     writer = csv.writer(file)
                     writer.writerow(["Rank", "Name", "Title", "Upvotes"])
                     for x in range(len(alist)):
-                        writer.writerow([x+1, str(alist[x]), str(lst[x]), str(lst2[x])])
+                        writer.writerow([x+1, str(alist[0][x]), str(alist[1][x]), str(alist[2][x])])
 
 
 
 def main():
     userinput = input("What subreddit would you like: ")
-    alist, lst, lst2 = scrapper(userinput)
-    writer(alist, lst, lst2)
+    alist = scrapper(userinput)
+    writer(alist)
     print("Done")
 
 main()
